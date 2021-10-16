fd58f681f59440a136578fee40113177614167fa
RachelNurmi91 <86205748+RachelNurmi91@users.noreply.github.com>
2021-10-15 19:46:33 -0500

    Update script.js
    Completed game. The text within the class p-response now replaces the previous text depending on your answer. Before this update the text was not being replaced. Instead it just kept adding additional text. Additionally, it has been adjusted so the game will not continue once you get the answer correct.

--- script.js
+++ script.js
@@ -10,10 +10,7 @@ document.getElementById("submitGuess").addEventListener("click", guessingGame)
 
 function guessingGame() {
     let userGuess = document.getElementById("userGuess").value;
-    let correct = document.createTextNode("Correct! Good Guess.");
-    let lower = document.createTextNode("Incorrect!\n\nHint: The first letter of your guess is alphabetically lower than that of the correct answer.");
-    let higher = document.createTextNode("Incorrect!\n\nHint: The first letter of your guess is alphabetically higher than that of the correct answer.");
-    let incorrect = document.createTextNode("That is not a valid choice.");
+    let paragraph = document.getElementById("p-response");
     let color1 = "#fff";
     let color2 = userGuess;
     let gradient = "linear-gradient(45deg"+","+color1+","+color2+")"; 
@@ -21,14 +18,20 @@ function guessingGame() {
     //Console Log for Debugging
     console.log(userGuess);
 
+
     if (COLORS_ARRAY.includes(userGuess) == false) {
-        document.getElementById("p-response").appendChild(incorrect);
+        paragraph.innerHTML = "That is not a valid choice.";
     } else if (userGuess < target ) {
-        document.getElementById("p-response").appendChild(lower);
+        paragraph.innerHTML = "Incorrect!<br>\
+        Hint: The first letter of your guess is alphabetically LOWER than that of the correct answer.";
     } else if (userGuess > target) {
-        document.getElementById("p-response").appendChild(higher);
+        paragraph.innerHTML = "Incorrect!<br>\
+        Hint: The first letter of your guess is alphabetically HIGHER than that of the correct answer.";
     } else {
-        document.getElementById("p-response").appendChild(correct);
+        paragraph.innerHTML = "Correct! Good Guess.";
         document.body.style.backgroundImage = gradient;
+        document.getElementById("submitGuess").removeEventListener("click", guessingGame)
     }
+
+    
 }
